4458c20c990eb4468ea2d2492ebb8174c4eefb60
zpskek <wjdghdwns0@gmail.com>
2021-01-18 16:00:59 +0900

    Models for user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_CHOICES = ((LANGUAGE_KOREAN, "Korean"), (LANGUAGE_ENGLISH, "English"))
+
+    CURRENCY_KRW = "krw"
+    CURRENCY_USD = "usd"
+    CURRENCY_CHOICES = ((CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD"))
+
+    avatar = models.ImageField()
+    bio = models.TextField(blank=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+    birthdate = models.DateField(null=True)
+    superhost = models.BooleanField(default=False)
