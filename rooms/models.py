dc9f462127bb7e885a3a96319ef89cd25467961d
zpskek <wjdghdwns0@gmail.com>
2021-01-18 17:24:34 +0900

    Create model of rooms

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """ RoomType Model Definition """
+
+    pass
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    pass
+
+
+class Room(core_models.TimeStampedModel):
+    name = models.CharField(max_length=140)
+    country = CountryField()
+    city = models.CharField(max_length=140)
+    address = models.CharField(max_length=140)
+    price = models.IntegerField()
+    guests = models.IntegerField()
+    bedrooms = models.IntegerField()
+    beds = models.IntegerField()
+    bathsrooms = models.IntegerField()
+    description = models.TextField()
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
