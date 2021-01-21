6d85abcee3a3bdbe5b45be4d6d0c2013942c84ee
zpskek <wjdghdwns0@gmail.com>
2021-01-21 11:36:43 +0900

    Correct spelling of room types from roome to room

--- rooms/management/commands/seed_room_types.py
+++ rooms/management/commands/seed_room_types.py
@@ -4,7 +4,7 @@ from rooms.models import RoomType
 
 class Command(BaseCommand):
 
-    help = "This command creates some roome types"
+    help = "This command creates some room types"
 
     def handle(self, *args, **options):
         room_types = ["Hotel Room", "Entire Place", "Shared Room", "Private Room"]
