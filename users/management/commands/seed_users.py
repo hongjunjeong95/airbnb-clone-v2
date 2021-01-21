c5d9dab604122df8270f864723da0fca1630fe23
zpskek <wjdghdwns0@gmail.com>
2021-01-21 10:37:32 +0900

    Implement create_command of amenity

--- users/management/commands/seed_users.py
+++ users/management/commands/seed_users.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django_seed import Seed
 from users.models import User
 
@@ -13,11 +13,9 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        try:
-            number = options.get("number", 1)
-            seeder = Seed.seeder()
-            seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False})
-            seeder.execute()
-            self.stdout.write(self.style.SUCCESS(f"Create {number} users"))
-        except CommandError:
-            print("error")
+
+        number = options.get("number", 1)
+        seeder = Seed.seeder()
+        seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False})
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"Create {number} users"))
