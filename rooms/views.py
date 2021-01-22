d01b1774b420047c5a2f31737d982cc73a0cc254
zpskek <wjdghdwns0@gmail.com>
2021-01-22 11:31:32 +0900

    Create footer.html

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,12 @@
+from datetime import datetime
 from django.shortcuts import render
 from . import models as room_models
 
 
 def HomeView(request):
     rooms = room_models.Room.objects.all()
-
-    return render(request, "pages/rooms/home.html", context={"rooms": rooms})
+    now = datetime.now()
+    year = datetime.date(now).year
+    return render(
+        request, "pages/rooms/home.html", context={"rooms": rooms, "year": year}
+    )
