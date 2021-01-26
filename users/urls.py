6e486eaa15b0d88aef9fa222d3c3f8444252d730
zpskek <wjdghdwns0@gmail.com>
2021-01-26 21:14:04 +0900

    Create Github login

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,12 @@ app_name = "users"
 urlpatterns = [
     path("signup/", user_views.SignUpView.as_view(), name="signup"),
     path("login/", user_views.LoginView.as_view(), name="login"),
+    path("login/github/", user_views.github_login, name="github-login"),
+    path(
+        "login/github/callback/",
+        user_views.github_login_callback,
+        name="github-callback",
+    ),
     path("logout/", user_views.log_out, name="logout"),
     path("<int:pk>/profile/", user_views.UserDetail, name="profile"),
     path("<int:pk>/update-profile/", user_views.UpdateProfile, name="update-profile"),
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
+import os
+import requests
+
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse, render
 from django.db.utils import IntegrityError
+from django.core.files.base import ContentFile
 from . import forms, models
 
 
@@ -65,6 +69,94 @@ class LoginView(FormView):
         if user is not None and user.email_verified == True:
             login(self.request, user)
         return super().form_valid(form)
+
+
+def github_login(request):
+    client_id = os.environ.get("GH_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback/"
+    scope = "read:user"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}"
+    )
+
+
+class GithubException(Exception):
+    pass
+
+
+def github_login_callback(request):
+    try:
+        code = request.GET.get("code", None)
+        if code is None:
+            raise GithubException("Can't get code")
+
+        client_id = os.environ.get("GH_ID")
+        client_secret = os.environ.get("GH_SECRET")
+
+        token_request = requests.post(
+            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+            headers={"Accept": "application/json"},
+        )
+        token_json = token_request.json()
+        error = token_json.get("error", None)
+
+        if error is not None:
+            raise GithubException("Can't get access token")
+
+        access_token = token_json.get("access_token")
+        profile_request = requests.get(
+            "https://api.github.com/user",
+            headers={
+                "Authorization": f"token {access_token}",
+                "Accept": "application/json",
+            },
+        )
+        profile_json = profile_request.json()
+        username = profile_json.get("login", None)
+
+        if username is None:
+            raise GithubException("Can't get username from profile_request")
+
+        avatar_url = profile_json.get("avatar_url", None)
+        if avatar_url is None:
+            raise GithubException("Can't get avatar_url from profile_request")
+
+        name = profile_json.get("name", None)
+        if name is None:
+            raise GithubException("Can't get name from profile_request")
+
+        email = profile_json.get("email", None)
+        print(email)
+        if email is None:
+            raise GithubException("Can't get email from profile_request")
+
+        bio = profile_json.get("bio", None)
+        if bio is None:
+            raise GithubException("Can't get bio from profile_request")
+
+        try:
+            user = models.User.objects.get(email=email)
+            if user.login_method != models.User.LOGIN_GITHUB:
+                raise GithubException(f"Please login with {user.login_method}")
+        except models.User.DoesNotExist:
+            user = models.User.objects.create(
+                username=email,
+                first_name=username,
+                email=email,
+                bio=bio,
+                login_method=models.User.LOGIN_GITHUB,
+                email_verified=True,
+            )
+            photo_request = requests.get(avatar_url)
+
+            user.avatar.save(f"{name}-avatar", ContentFile(photo_request.content))
+            user.set_unusable_password()
+            user.save()
+            login(request, user)
+            return redirect(reverse("core:home"))
+    except GithubException as error:
+        print(error)
+        return redirect(reverse("core:home"))
 
 
 def log_out(request):
