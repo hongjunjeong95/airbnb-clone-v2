4543f248fece73fc574237dd67126504ecff4da8
zpskek <wjdghdwns0@gmail.com>
2021-01-31 16:33:13 +0900

    Resize layout on home

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,12 @@ module.exports = {
       },
       gridAutoRows: {
         "400px": "minmax(0, 400px)",
+        "300px": "minmax(0, 300px)",
       },
       gridTemplateRows: {
         "2-400px": "repeat(2, 400px)",
         "2-200px": "repeat(2, 200px)",
+        "2-300px": "repeat(2, 300px)",
       },
     },
   },
