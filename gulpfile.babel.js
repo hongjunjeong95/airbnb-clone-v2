bb1a0e2951ee734ba2ae27f5d1d78c5c40dc1063
zpskek <wjdghdwns0@gmail.com>
2021-02-06 15:59:00 +0900

    Move script from base.html to translator.js file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,12 +4,20 @@ import sass from "gulp-sass";
 import csso from "gulp-csso";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import babelify from "babelify";
+import bro from "gulp-bro";
+import del from "del";
 
 const path = {
   scss: {
     src: "assets/scss/styles.scss",
     dest: "static/css",
     watch: "assets/scss/**/*.scss",
+  },
+  js: {
+    src: "assets/js/main.js",
+    dest: "static/js",
+    watch: "assets/js/**/*.js",
   },
 };
 
@@ -23,8 +31,23 @@ export const css = () => {
     .pipe(gulp.dest(path.scss.dest));
 };
 
+const js = () =>
+  gulp
+    .src(path.js.src)
+    .pipe(
+      bro({
+        transform: [babelify.configure({ presets: ["@babel/preset-env"] })],
+      })
+    )
+    .pipe(gulp.dest(path.js.dest));
+
 const watch = () => {
   gulp.watch(path.scss.watch, css);
+  gulp.watch(path.js.watch, js);
 };
 
-export const dev = gulp.series([css, watch]);
+const clean = () => del(["static/css/", "static/js/"]);
+const assets = gulp.series([css, js]);
+
+export const build = gulp.series([clean, assets]);
+export const dev = gulp.series([build, watch]);
